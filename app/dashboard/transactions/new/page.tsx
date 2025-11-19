0e7e272f902b0acc2d639c1f76526ac6c8dee64d
andriinakonechnyi.work <andriinakonechnyi.work@gmail.com>
2025-11-19 21:05:50 +0200

    add transaction page

---
+++ app/dashboard/transactions/new/page.tsx
@@ -0,0 +1,42 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+
+export default function NewTransactionPage() {
+  return (
+    <div className="mx-auto max-w-screen-xl py-10">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard/transactions">Transactions</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>New transactions</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <Card className="mt-4 max-w-screen-md">
+        <CardHeader>
+          <CardTitle>New Transaction</CardTitle>
+        </CardHeader>
+        <CardContent>Tra</CardContent>
+      </Card>
+    </div>
+  );
+}
