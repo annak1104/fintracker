86c388d54692e20c07b26e9df5f8c1915a3064da
andriinakonechnyi.work <andriinakonechnyi.work@gmail.com>
2025-11-25 16:04:45 +0200

    create edittransactionPage

--- app/dashboard/transactions/[transactionId]/page.tsx
+++ app/dashboard/transactions/[transactionId]/page.tsx
@@ -1,8 +1,57 @@
-export default async function TransactionIdPage({
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getCatgories } from "@/data/getCategories";
+import Link from "next/link";
+
+export default async function EditTransactionPage({
   params,
 }: {
   params: Promise<{ transactionId: string }>;
 }) {
   const paramsValues = await params;
-  return <div>transaction {paramsValues.transactionId}</div>;
+  const transactionId = Number(paramsValues.transactionId);
+
+  if (isNaN(transactionId)) {
+    return <div>Ooops, transaction not found</div>;
+  }
+
+  const categories = await getCatgories();
+  return (
+    <div className="mx-auto max-w-7xl py-10">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard/transactions">Transactions</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Edit transactions</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <Card className="mt-4 max-w-3xl">
+        <CardHeader>
+          <CardTitle>Edit Transaction</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {/* <NewTransactionForm categories={categories} /> */}
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
