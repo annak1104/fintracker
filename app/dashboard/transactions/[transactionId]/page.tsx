98ff3e15b71d4735e5ce4146b5a6d6174dda72a4
andriinakonechnyi.work <andriinakonechnyi.work@gmail.com>
2025-11-25 20:22:34 +0200

    Add custom error page for non-existent transactions

--- app/dashboard/transactions/[transactionId]/page.tsx
+++ app/dashboard/transactions/[transactionId]/page.tsx
@@ -1,15 +1,6 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getCategories } from "@/data/getCategories";
 import getTransaction from "@/data/getTransaction";
-import Link from "next/link";
 import { notFound } from "next/navigation";
 import { DeleteTransactionDialog } from "./delete-transactions-dialog";
 import EditTransactionForm from "./edit-transaction-form";
@@ -33,43 +24,22 @@ export default async function EditTransactionPage({
     notFound();
   }
   return (
-    <div className="mx-auto max-w-7xl py-10">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="/dashboard">Dashboard</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="/dashboard/transactions">Transactions</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Edit transaction</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-      <Card className="mt-4 max-w-3xl">
-        <CardHeader>
-          <CardTitle className="flex justify-between">
-            <span>Edit Transaction</span>
-            <DeleteTransactionDialog
-              transactionId={transaction.id}
-              transactionDate={transaction.transactionDate}
-            />
-          </CardTitle>
-        </CardHeader>
-        <CardContent>
-          <EditTransactionForm
-            transaction={transaction}
-            categories={categories}
+    <Card className="mt-4 max-w-3xl">
+      <CardHeader>
+        <CardTitle className="flex justify-between">
+          <span>Edit Transaction</span>
+          <DeleteTransactionDialog
+            transactionId={transaction.id}
+            transactionDate={transaction.transactionDate}
           />
-        </CardContent>
-      </Card>
-    </div>
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <EditTransactionForm
+          transaction={transaction}
+          categories={categories}
+        />
+      </CardContent>
+    </Card>
   );
 }
