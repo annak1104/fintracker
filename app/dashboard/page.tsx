5d28e573b813f96655629a6d69bd948cdf42b5e9
andriinakonechnyi.work <andriinakonechnyi.work@gmail.com>
2025-11-26 12:07:11 +0200

    Add the recent transactions table

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,10 @@
-import Link from "next/link";
+import RecentTransactions from "./recent-transactions";
 
 export default function DashboardPage() {
-  return <Link href="/dashboard/transactions/new">New transaction</Link>;
+  return (
+    <div className="mx-auto max-w-7xl py-5">
+      <h1 className="pb-5 text-4xl font-semibold">Dashboard</h1>
+      <RecentTransactions />
+    </div>
+  );
 }
